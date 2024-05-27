1649751ef664cc9f24259ea9e221d8cbe8258211
fa18kouki <kouki0180@outlook.jp>
2024-05-27 17:37:01 +0900

    env-file

--- backend/auth/auth_utils.py
+++ backend/auth/auth_utils.py
@@ -1,17 +1,33 @@
+import os
+from dotenv import load_dotenv
 import firebase_admin
 from firebase_admin import credentials, auth
+import json
+
+load_dotenv()
+
+def initialize_firebase():
+    if not firebase_admin._apps:
+        firebase_credentials = {
+            "type": os.getenv('FIREBASE_TYPE'),
+            "project_id": os.getenv('FIREBASE_PROJECT_ID'),
+            "private_key_id": os.getenv('FIREBASE_PRIVATE_KEY_ID'),
+            "private_key": os.getenv('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
+            "client_email": os.getenv('FIREBASE_CLIENT_EMAIL'),
+            "client_id": os.getenv('FIREBASE_CLIENT_ID'),
+            "auth_uri": os.getenv('FIREBASE_AUTH_URI'),
+            "token_uri": os.getenv('FIREBASE_TOKEN_URI'),
+            "auth_provider_x509_cert_url": os.getenv('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
+            "client_x509_cert_url": os.getenv('FIREBASE_CLIENT_X509_CERT_URL')
+        }
+        cred = credentials.Certificate(firebase_credentials)
+        firebase_admin.initialize_app(cred)
 
 def get_authenticated_user_details(request_headers):
-    # Firebase Admin SDKの初期化
-    if not firebase_admin._apps:
-        cred = credentials.Certificate('./egt-gpt-firebase-adminsdk-w0ifw-89672149e0.json')
-        firebase_admin.initialize_app(cred)
-    
+    initialize_firebase()
     user_object = {}
-    print("Requesfdlkjhasf;lka: ", request_headers)
-    ## check the headers for the Principal-Id (the guid of the signed in user)
+    print("Request headers: ", request_headers)
     if "Authorization" not in request_headers.keys():
-        ## if it's not, assume we're in development mode and return a default user
         from . import sample_user
         raw_user_object = sample_user.sample_user
         user_object = raw_user_object
@@ -22,9 +38,7 @@ def get_authenticated_user_details(request_headers):
     return user_object
 
 def fetch_users():
-    if not firebase_admin._apps:
-        cred = credentials.Certificate('./egt-gpt-firebase-adminsdk-w0ifw-89672149e0.json')
-        firebase_admin.initialize_app(cred)
+    initialize_firebase()
     users = []
     page = auth.list_users()
     while page:
@@ -34,7 +48,5 @@ def fetch_users():
     return users
 
 def get_user(uid):
-    if not firebase_admin._apps:
-        cred = credentials.Certificate('./egt-gpt-firebase-adminsdk-w0ifw-89672149e0.json')
-        firebase_admin.initialize_app(cred)
+    initialize_firebase()
     return auth.get_user(uid).__dict__
