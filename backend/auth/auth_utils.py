125e3f4d5e7b4e860b79db9820bad3d115e74afa
fa18kouki <kouki0180@outlook.jp>
2024-05-27 19:14:59 +0900

    firebase_credentiasl

--- backend/auth/auth_utils.py
+++ backend/auth/auth_utils.py
@@ -12,7 +12,7 @@ def initialize_firebase():
             "type": os.getenv('FIREBASE_TYPE'),
             "project_id": os.getenv('FIREBASE_PROJECT_ID'),
             "private_key_id": os.getenv('FIREBASE_PRIVATE_KEY_ID'),
-            "private_key": os.getenv('FIREBASE_PRIVATE_KEY'),
+            "private_key": os.getenv('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
             "client_email": os.getenv('FIREBASE_CLIENT_EMAIL'),
             "client_id": os.getenv('FIREBASE_CLIENT_ID'),
             "auth_uri": os.getenv('FIREBASE_AUTH_URI'),
@@ -20,6 +20,7 @@ def initialize_firebase():
             "auth_provider_x509_cert_url": os.getenv('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
             "client_x509_cert_url": os.getenv('FIREBASE_CLIENT_X509_CERT_URL')
         }
+        print("Firebase credentials: ", firebase_credentials)
         cred = credentials.Certificate(firebase_credentials)
         firebase_admin.initialize_app(cred)
 
